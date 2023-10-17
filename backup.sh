22af6d89734e32bf60a83ea611696ae41b46ced0
zhz <me@zhz.moe>
2023-10-17 23:37:23 +0800

    fix .sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mc alias set backup_s3 "$S3_ENDPOINT" "$S3_ACCESS_KEY" "$S3_SECRET_KEY"
-pg_dump -Fc -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" > /backup/"$BU_FILE_NAME".dump
+pg_dump -Fc -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" > /backup/"$BU_FILE_NAME"-"$(date +%Y-%m-%d)".dump
 mc cp --recursive /backup/"$BU_FILE_NAME"-"$(date +%Y-%m-%d)".dump backup_s3/"${S3_BUCKET}"/"${S3_PREFIX}"
 exit 0
